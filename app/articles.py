37781a1ebb02da43b744f27dfe16952da59b2d0f
steve choi <steve.choi@telus.com>
2022-06-18 15:00:20 -0400

    refactor: 💡 separate articles.py from main

---
+++ app/articles.py
@@ -0,0 +1,61 @@
+import os
+import logging
+
+logger = logging.getLogger('app.publisher')
+
+class Articles:
+    def __init__(self, bigquery_client):
+        """Instantiates the Articles class for retrieving articles from BigQuery
+        Args:
+            bigquery_client: pass existing BigQuery client as an argument
+        """
+        self.bigquery_client = bigquery_client
+
+    def get_articles(self, user_id):
+        """
+        Args:
+            user_id: User ID from the browser cookie
+        Returns:
+            articles: List of dictionaries containing article data
+        """
+        latest_articles_table = os.getenv('ARTICLES_TABLE')
+        personalized_articles_table = os.getenv('PERSONALIZED_ARTICLES_TABLE')
+        articles_query = """
+            WITH latest_articles AS (
+                SELECT
+                    * EXCEPT (load_timestamp, article_order)
+                FROM `{}`
+            ),
+            personalized_articles AS (
+                SELECT
+                    'personalized' AS sort,
+                    * EXCEPT (user_id, topic, total_clicks, user_already_clicked, article_order, load_timestamp)
+                FROM `{}`
+                WHERE
+                    user_id = '{}'
+                    AND user_already_clicked = FALSE
+                ORDER BY
+                    total_clicks DESC, publishedAt DESC
+                LIMIT 10
+            )
+            SELECT * FROM latest_articles
+            UNION ALL
+            SELECT * FROM personalized_articles
+        """.format(latest_articles_table, personalized_articles_table, user_id)
+        
+        print('running query: {}'.format(articles_query))
+        logger.info('running query: {}'.format(articles_query))
+
+        # run query and store results in list of dictionaries
+        query_job = self.bigquery_client.query(articles_query)
+        articles = []
+        for row in query_job:
+            articles.append(dict(row))
+
+        # convert datetime objects to strings
+        for index, article in enumerate(articles):
+            article['publishedAt'] = article['publishedAt'].strftime('%Y-%m-%d %H:%M:%S')
+
+        return articles
+
+    
--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
-import requests
 import os
-import uuid
-import json
-from flask import Flask, request, render_template, session, make_response, after_this_request, redirect
+from flask import Flask, render_template, make_response, redirect
 import logging
 from tracking import check_or_set_user_id, count_hits, track_click_and_get_url, track_impressions
+from articles import Articles
 from google.cloud import pubsub, bigquery
 import google.auth
-from google.auth import impersonated_credentials
 
 app = Flask(__name__)
 
@@ -34,63 +31,23 @@ def home():
 
     # count hits for the current user ID
     user_hits = count_hits()
-
-    # instantiate global articles for tracking default and random
-    global articles
-    articles = []
-
-    # get latest articles from bigquery
-    latest_articles_table = os.getenv('ARTICLES_TABLE')
-    personalized_articles_table = os.getenv('PERSONALIZED_ARTICLES_TABLE')
-    articles_query = """
-        WITH latest_articles AS (
-            SELECT
-                * EXCEPT (load_timestamp, article_order)
-            FROM `{}`
-        ),
-        personalized_articles AS (
-            SELECT
-                'personalized' AS sort,
-                * EXCEPT (user_id, topic, total_clicks, user_already_clicked, article_order, load_timestamp)
-            FROM `{}`
-            WHERE
-                user_id = '{}'
-                AND user_already_clicked = FALSE
-            ORDER BY
-                total_clicks DESC, publishedAt DESC
-            LIMIT 10
-        )
-        SELECT * FROM latest_articles
-        UNION ALL
-        SELECT * FROM personalized_articles
-    """.format(latest_articles_table, personalized_articles_table, user_id)
     
-    print('running query: {}'.format(articles_query))
-    logger.info('running query: {}'.format(articles_query))
-
-    # run query and store results in list of dictionaries
-    query_job = bigquery_client.query(articles_query)
-    for row in query_job:
-        articles.append(dict(row))
-
-    # # convert datetime objects to strings for default
-    for i in articles:
-        for field in i:
-            if field == 'publishedAt':
-                i['publishedAt'] = i['publishedAt'].strftime('%Y-%m-%d %H:%M:%S')
+    # instantiate global articles for tracking and retrieve list of article dictionaries
+    global articles
+    articles_client = Articles(bigquery_client)
+    articles = articles_client.get_articles(user_id)
 
     # filter articles based on sort field
-    latest_articles = [d for d in articles if d['sort'] == 'latest']
-    popular_articles = [d for d in articles if d['sort'] == 'popular']
-    random_articles = [d for d in articles if d['sort'] == 'random']
-    personalized_articles = [d for d in articles if d['sort'] == 'personalized']
+    latest_articles = [a for a in articles if a['sort'] == 'latest']
+    popular_articles = [a for a in articles if a['sort'] == 'popular']
+    random_articles = [a for a in articles if a['sort'] == 'random']
+    personalized_articles = [a for a in articles if a['sort'] == 'personalized']
 
+    # use popular articles if personalized articles is empty
+    personalized_articles = popular_articles if not personalized_articles else personalized_articles
+    
     # track article impressions
     track_impressions(gcp_project_id, pubsub_client, articles, user_id)
-
-    # Check if articles are empty
-    if (len(personalized_articles) < 1):
-        personalized_articles = popular_articles
 
     # create flask response
     resp = make_response(
